93184e07b62ae2299faa7b3f863b8983f3210dc3
Alex M <milesAwayAlex@gmail.com>
2021-08-17 00:34:57 -0700

    Add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts 'Total annual revenue'
+puts Store.sum('annual_revenue')
+
+puts 'Avg annual revenue'
+puts Store.average('annual_revenue')
+
+puts 'Num of high-revenue stores'
+puts Store.where('annual_revenue > ?', 1000000).count
