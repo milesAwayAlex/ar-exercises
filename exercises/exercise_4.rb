dec95987fdbc7573be9b1984a4cc96f6e57b798d
Alex M <milesAwayAlex@gmail.com>
2021-08-17 00:27:51 -0700

    Add exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,36 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+[{name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true},
+ {name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false},
+ {name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true}].each do |s|
+    Store.create(
+      name: s[:name],
+      annual_revenue: s[:annual_revenue],
+      mens_apparel: s[:mens_apparel],
+      womens_apparel: s[:womens_apparel])
+  end
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |s|
+  puts s.name
+  puts s.annual_revenue
+end
+
+Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000).each do |s|
+  puts s.name
+  puts s.annual_revenue
+end
